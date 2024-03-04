dafd53a77fb8ff714df79377237052d8fec5eeef
Sebastian Quito <josue.quito@udla.edu.ec>
2024-03-04 14:08:22 -0500

    Created basic components

---
+++ components/CustomButton.tsx
@@ -0,0 +1,14 @@
+function CustomButton() {
+    return (
+        <button
+            disabled={false}
+            type={"button"}
+            className={`custom-btn`}
+            onClick={() => {}}
+        >
+
+        </button>
+    )
+}
+
+export default CustomButton
