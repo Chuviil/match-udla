e2140cf72dc19562730c14f1c1314f4c4a613b49
Sebastian Quito <josue.quito@udla.edu.ec>
2024-03-05 21:24:09 -0500

    Modified page.tsx and layout.tsx to use components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
-  return (
-    <main className="overflow-hidden">
+import {Hero} from "@/components";
 
-    </main>
-  );
+export default function Home() {
+    return (
+        <main className="overflow-hidden">
+            <Hero/>
+        </main>
+    );
 }
