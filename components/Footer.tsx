cdd4a53b8470a598f2ca8c4569a416e5d1e65c90
Sebastian Quito <josue.quito@udla.edu.ec>
2024-03-05 22:05:14 -0500

    Fix: NavBar and Footer now match Clubes Udla page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,8 +31,8 @@ const Footer = () => {
             </div>
         </footer>*/
         <footer className={"flex flex-col mt-5 border-t border-black-100 bg-black-200 text-white"}>
-            <div className={"flex flex-col justify-between items-center sm:px-16 px-6 py-10"}>
-                <Image src={"/udla_logo_blanco.png"} alt={"Udla Logo"} width={420} height={50}
+            <div className={"flex flex-col justify-between gap-5 items-center sm:px-16 px-6 py-10"}>
+                <Image src={"/udla_logo_blanco.png"} alt={"Udla Logo"} width={236} height={105}
                        className={"object-contain"}/>
                 <div className={"flex flex-row gap-5"}>
                     <Link href={"https://www.facebook.com/clubesudla"} target={"_blank"}>
@@ -46,9 +46,19 @@ const Footer = () => {
                         </div>
                     </Link>
                 </div>
+                <div className={"text-center gap-2 flex flex-col"}>
+                    <p>David Flores Larrea</p>
+                    <p>Coordinador de Clubes</p>
+                    <Link href={"mailto:david.flores@udla.edu.ec"}>
+                        david.flores@udla.edu.ec
+                    </Link>
+                    <Link href={"mailto:clubes@udla.edu.ec"}>
+                        clubes@udla.edu.ec
+                    </Link>
+                </div>
             </div>
             <div
-                className={"flex justify-between items-center flex-wrap mt-10 border-t border-black-100 sm:px-16 px-6 py-10"}>
+                className={"flex justify-between items-center flex-wrap border-t border-black-100 sm:px-16 px-6 py-10"}>
                 <div className={"flex-1 flex sm:justify-start justify-center max-sm:mt-4 gap-10"}>
                     <p>@ 2024 Clubes UDLA</p>
                 </div>
