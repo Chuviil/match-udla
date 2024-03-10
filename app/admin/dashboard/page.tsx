5d2362aba1359b7f06585bd74bbd8a66896ba784
Sebastian Quito <josue.quito@udla.edu.ec>
2024-03-09 20:17:33 -0500

    Created a placeholder route to test authentication middleware verification

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div className={"py-36"}>
+            <h1>DASHBOARD HERE :D</h1>
+        </div>
+    );
+};
+
+export default Page;
