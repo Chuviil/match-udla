bdb04382cac85937e11b5de28dbeaad0f516ab05
Sebastian Quito <josue.quito@udla.edu.ec>
2024-03-12 15:30:14 -0500

    Initial setup for shadcn/ui components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,98 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    mode: "jit",
-    theme: {
-        extend: {
-            fontFamily: {
-                inter: ["Inter", "sans-serif"],
-            },
-            colors: {
-                "black": {
-                    100: "#2B2C35",
-                    200: "#191919",
-                    DEFAULT: "#000"
-                },
-                "primary-blue": {
-                    DEFAULT: "#2B59FF",
-                    100: "#F5F8FF",
-                },
-                "secondary-orange": "#f79761",
-                "light-white": {
-                    DEFAULT: "rgba(59,60,152,0.03)",
-                    100: "rgba(59,60,152,0.02)",
-                },
-                grey: "#747A88",
-            },
-            backgroundImage: {
-                'hero-bg': "url('/futbol_bg.png')"
-            }
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  mode: "jit",
+  theme: {
+    /*container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },*/
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        "black": {
+          100: "#2B2C35",
+          200: "#191919",
+          DEFAULT: "#000"
+        },
+        "primary-blue": {
+          DEFAULT: "#2B59FF",
+          100: "#F5F8FF",
+        },
+        "secondary-orange": "#f79761",
+        "light-white": {
+          DEFAULT: "rgba(59,60,152,0.03)",
+          100: "rgba(59,60,152,0.02)",
+        },
+        grey: "#747A88",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      backgroundImage: {
+        'hero-bg': "url('/futbol_bg.png')"
+      }
     },
-    plugins: [],
-};
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
