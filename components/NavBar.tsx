5ca713ba75cd328eb0cf0570bc28c2b901a1ccd7
Sebastian Quito <josue.quito@udla.edu.ec>
2024-03-13 01:56:56 -0500

    Fix: Navbar is now more responsive

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 const NavBar = () => {
     return (
         <header className={"w-full fixed z-10 bg-black-200"}>
-            <nav className={"max-w[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4"}>
+            <nav className={"max-w[1440px] mx-auto flex justify-center sm:justify-between items-center sm:px-16 px-6 py-4"}>
                 <Link href={"/"} className={"flex justify-center items-center"}>
                     <Image src={"/logo_clubes_udla_blanco.png"} alt={"Udla Logo"} width={115} height={51}
                            className={"object-contain"}/>
                 </Link>
-                <div className={"flex flex-row gap-5 font-bold"}>
+                <div className={"sm:flex flex-row gap-5 font-bold hidden"}>
                     <Link href={"/"} className={"navbar__link"}>
                         Inicio
                     </Link>
