71c58f8545e896695d26a966e92e9267cf4acc48
Sebastian Quito <josue.quito@udla.edu.ec>
2024-03-22 00:16:20 -0500

    Created some functions to handle queryFiltering, data retrieval, data update and data deletion

--- lib/firebase/firestore.ts
+++ lib/firebase/firestore.ts
@@ -1,10 +1,57 @@
-import {addDoc, collection} from "firebase/firestore";
+import {addDoc, collection, deleteDoc, doc, getDocs, query, Query, updateDoc, where} from "firebase/firestore";
 import {db} from "./firebase";
+import {Reservation, ReservationDTO, ReservationStatus} from "@/types";
 
 export async function createReservation(data: any) {
     const docRef = await addDoc(
         collection(db, "reservas"),
-        {...data, createdAt: new Date(), estado: "PENDIENTE"}
+        {...data, createdAt: new Date(), estado: ReservationStatus.PENDIENTE}
     );
     return docRef.id;
+}
+
+interface ReservationQueryFilters {
+    estado?: ReservationStatus,
+    date?: Date
+}
+
+function applyQueryFilters(q: Query, {date, estado}: ReservationQueryFilters): Query {
+    if (date) {
+        q = query(q, where("fechaReserva", "==", date));
+    }
+    if (estado) {
+        q = query(q, where("estado", "==", estado));
+    }
+    return q;
+}
+
+export async function getReservations(filters?: ReservationQueryFilters): Promise<Reservation[]> {
+    let q = query(collection(db, "reservas"));
+
+    if (filters) {
+        q = applyQueryFilters(q, filters)
+    }
+
+    const results = await getDocs(q);
+    return results.docs.map(doc => {
+        const data = doc.data() as ReservationDTO;
+        return {
+            ...data,
+            id: doc.id,
+            createdAt: data.createdAt.toDate(),
+            fechaReserva: data.fechaReserva.toDate(),
+        };
+    });
+}
+
+export async function deleteReservation(reservationId: string) {
+    await deleteDoc(doc(db, "reservas", reservationId));
+    return reservationId;
+}
+
+export async function updateReservationStatus(reservationId: string, status: string) {
+    const reservationRef = doc(db, "reservas", reservationId);
+    if (reservationRef) {
+        await updateDoc(reservationRef, {estado: status});
+    }
 }
